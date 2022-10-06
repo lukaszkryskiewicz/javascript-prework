d19b0a927fc9adfcdf7c98f5aa57b83322713ea9
Lukasz Kryskiewicz <lukasz.kryskiewicz@gmail.com>
2022-10-06 21:25:57 +0200

    Add getMoveName and displayResult in script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,35 @@
+function getMoveName(argMoveId){
+    if(argMoveId == 1){
+      return 'kamień';
+    } else if (argMoveId == 2){
+        return 'papier';
+    } else if (argMoveId == 3){
+        return 'nożyce';
+    } else {
+      printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+      return 'nieznany ruch';
+    }
+  }
+
+function displayResult(argComputerMove, argPlayerMove){
+    console.log('moves:', argComputerMove, argPlayerMove);
+    if( argComputerMove == argPlayerMove){
+        return 'Remis!';
+    }   else if (( argComputerMove == 'kamień' && argPlayerMove == 'papier') || ( argComputerMove =='papier' && argPlayerMove == "nożyce") || ( argComputerMove =='nożyce' && argPlayerMove == "kamień")){
+        return 'Ty wygrywasz!';
+    }   else if ( ( argComputerMove == 'kamień' && argPlayerMove == 'nożyce') || ( argComputerMove =='papier' && argPlayerMove == "kamień") || ( argComputerMove =='nożyce' && argPlayerMove == "papier")){
+        return 'Wygrywa komputer!';
+    }   else {
+        return 'Możesz podać tylko liczbę 1 , 2 lub 3!!';
+    }
+}
+
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
-
-
-if (randomNumber == 1){
-    computerMove = 'kamień';
-}   else if (randomNumber == 2){
-    computerMove = 'papier';
-}   else if (randomNumber == 3){
-    computerMove = 'nożyce';
-}
+let computerMove = getMoveName(randomNumber);
 
 printMessage('Mój ruch to: ' + computerMove);
 
@@ -19,25 +37,8 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
-
-if(playerInput == '1'){
-  playerMove = 'kamień';
-}   else if (playerInput == 2){
-    playerMove = 'papier';
-}   else if (playerInput == 3){
-    playerMove = 'nożyce';
-}
+let playerMove = getMoveName(playerInput);
 
 printMessage('Twój ruch to: ' + playerMove);
 
-if( computerMove == playerMove){
-    printMessage('Remis!');
-}   else if (( computerMove == 'kamień' && playerMove == 'papier') || ( computerMove =='papier' && playerMove == "nożyce") || ( computerMove =='nożyce' && playerMove == "kamień")){
-    printMessage('Ty wygrywasz!');
-}   else if ( ( computerMove == 'kamień' && playerMove == 'nożyce') || ( computerMove =='papier' && playerMove == "kamień") || ( computerMove =='nożyce' && playerMove == "papier")){
-    printMessage('Wygrywa komputer!');
-}   else {
-    printMessage('Możesz podać tylko liczbę 1 , 2 lub 3!!')
-}
-
+printMessage(displayResult(computerMove,playerMove));
